659d87769bdabf1277e75e28dc0a06affc5a1b3a
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-12-21 00:26:42 +0300

    chore: replace namespace 'SearchProcess' with object

--- src/processes/search/steps.ts
+++ src/processes/search/steps.ts
@@ -6,67 +6,65 @@ import { Story } from "@/shared/types/story"
 import { sendStoryCard } from "@/view/story"
 import { PaginationData, SelectedPageData } from "@/shared/types/pagination"
 
-export namespace SearchProcess {
-   export async function getPreliminaryStoriesData(
-      query: string | null,
-   ): Promise<PaginationData> {
-      try {
-         const { totalCount } = await storyService.getStories({
-            search: query,
-            only_count: true,
-            limit: pageLimit,
-         })
+export const SearchProcess = { getPreliminaryStoriesData, sendSearchResult, sendPage }
 
-         const pageCount = calculatePages(totalCount, pageLimit)
+async function getPreliminaryStoriesData(query: string | null): Promise<PaginationData> {
+   try {
+      const { totalCount } = await storyService.getStories({
+         search: query,
+         only_count: true,
+         limit: pageLimit,
+      })
 
-         return { totalCount, pageCount }
-      } catch (error) {
-         throw new Error(error)
-      }
+      const pageCount = calculatePages(totalCount, pageLimit)
+
+      return { totalCount, pageCount }
+   } catch (error) {
+      throw new Error(error)
    }
+}
 
-   export async function sendPage(chatId: number, query: string | null, pageNumber: number) {
-      try {
-         const storiesResponse = await storyService.getStories({
-            search: query,
-            limit: pageLimit,
-            page: pageNumber - 1,
-         })
-         const stories: Story[] = storiesResponse.data
+async function sendPage(chatId: number, query: string | null, pageNumber: number) {
+   try {
+      const storiesResponse = await storyService.getStories({
+         search: query,
+         limit: pageLimit,
+         page: pageNumber - 1,
+      })
+      const stories: Story[] = storiesResponse.data
 
-         if (stories.length > 0) {
-            stories.forEach(async (story) => await sendStoryCard(chatId, story))
-         } else {
-            await BOT.sendMessage(chatId, "0️⃣ No stories found on this page.")
-         }
-      } catch (error) {
+      if (stories.length > 0) {
+         stories.forEach(async (story) => await sendStoryCard(chatId, story))
+      } else {
+         await BOT.sendMessage(chatId, "0️⃣ No stories found on this page.")
+      }
+   } catch (error) {
+      await BOT.sendMessage(
+         chatId,
+         "❌ An error occurred while fetching results. Please try again.",
+      )
+      console.error("Error fetching results:", error)
+   }
+}
+
+async function sendSearchResult(
+   chatId: number,
+   query: string | null,
+   pageCount: number,
+   selectedPageData: SelectedPageData,
+) {
+   switch (selectedPageData.data) {
+      case "validPage":
+         await sendPage(chatId, query, selectedPageData.page)
+         break
+      case "invalidPage":
          await BOT.sendMessage(
             chatId,
-            "❌ An error occurred while fetching results. Please try again.",
+            `⚠️ Invalid page number. Please enter a number between 1 and ${pageCount}.`,
          )
-         console.error("Error fetching results:", error)
-      }
-   }
-
-   export async function sendSearchResult(
-      chatId: number,
-      query: string | null,
-      pageCount: number,
-      selectedPageData: SelectedPageData,
-   ) {
-      switch (selectedPageData.data) {
-         case "validPage":
-            await sendPage(chatId, query, selectedPageData.page)
-            break
-         case "invalidPage":
-            await BOT.sendMessage(
-               chatId,
-               `⚠️ Invalid page number. Please enter a number between 1 and ${pageCount}.`,
-            )
-            break
-         case "cancelled":
-            await BOT.sendMessage(chatId, "⏹️ Search cancelled.")
-            break
-      }
+         break
+      case "cancelled":
+         await BOT.sendMessage(chatId, "⏹️ Search cancelled.")
+         break
    }
 }
